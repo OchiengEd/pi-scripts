a19b8b4c11be38247b62f06b068081ac5e1025ca
Edmund Ochieng <ochienged@gmail.com>
2016-04-23 22:43:33 +0000

    Update main function

--- switch.py
+++ switch.py
@@ -5,29 +5,45 @@ import RPi.GPIO as GPIO
 
 class Switch:
 
- """
- def __init__(self):
+ def __init__(self, switchPIN):
   GPIO.setmode(GPIO.BCM)
-  switchPIN = 17 # Pin 11 on the Pi
+  #GPIO.setwarnings(False)
+  self.switchPIN = switchPIN
   GPIO.setup(switchPIN, GPIO.OUT)
-  """
 
  """
  Turn GPIO port high
  """
- def on(self, GPIO_PIN):
-  #GPIO.output(GPIO_PIN, GPIO.HIGH)
+ def on(self):
+  GPIO.output(self.switchPIN, GPIO.HIGH)
   print("Turn on")
 
  """
  Turn GPIO port low
  """
- def off(self, GPIO_PIN):
-  #GPIO.output(GPIO_PIN, GPIO.LOW)
+ def off(self):
+  GPIO.output(self.switchPIN, GPIO.LOW)
   print("Turn off")
  
 
+class Garage(Switch):
+
+ def __init(selfi, PIN):
+  Switch.__init__(self, PIN)
+
+ def switcher(self):
+  self.on()
+  time.sleep(0.5)
+  self.off()
+
+
 if __name__ == "__main__":
- switch = Switch()
- switch.on(17)
+ garage = Garage(17)
+
+ try:
+  garage.switcher()
+ except KeyboardInterrupt:
+  print("Interrupt received.Shutting down")
+ finally:
+  GPIO.cleanup()
  print("Task completed!")
